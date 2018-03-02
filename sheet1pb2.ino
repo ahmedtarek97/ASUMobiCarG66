c3dcf8942d255a80b691c9de6ab66cbbbf3dfc6a
Ahmad_Abdelsalam <31441716+AhmadAbdelsalam@users.noreply.github.com>
2018-03-02 14:43:00 -0800

    Add files via upload

---
+++ sheet1pb2.ino
@@ -0,0 +1,27 @@
+
+ int trigPin = 9;
+ int echoPin = 10;
+ long duration;
+ int distance;
+void setup() {
+     pinMode(trigPin, OUTPUT);
+     pinMode(echoPin, INPUT); 
+     Serial.begin(9600); 
+}
+void loop() {
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+
+Serial.print("Distance: ");
+Serial.println(distance);
+delay(2000);
+}
