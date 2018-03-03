6255f7682a4dfcf97e6ee4a6ab116210b19e5332
AhmedGalal21 <ahmedmedo867@gmail.com>
2018-03-03 15:12:14 +0200

    Update RC car.ino

--- Team 1/RC car.ino
+++ Team 1/RC car.ino
@@ -2,6 +2,7 @@ int motorPin1 = 10;
 int motorPin2 = 11;
 int motorPin3 = 5;
 int motorPin4 = 6;
+char data = 0; 
 
 void setup() {
  
@@ -11,8 +12,22 @@ void setup() {
  pinMode(motorPin3,OUTPUT); 
  pinMode(motorPin4,OUTPUT); 
 
-  
-}
 
 void loop() {
+ 
+ 
+  if (Serial.available() > 0) 
+{ 
+ data = Serial.read(); 
+   if(data == '1')        //move forwards 
+ { 
+   digitalWrite(motorPin1,HIGH); 
+   digitalWrite(motorPin2,LOW); 
+   digitalWrite(motorPin3,HIGH); 
+   digitalWrite(motorPin4,LOW); 
+ } 
+ 
+
+  
+}
 }
