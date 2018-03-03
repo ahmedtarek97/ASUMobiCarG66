df97a480c838b5754c96f1af16f9f398475e7b44
AhmedGalal21 <ahmedmedo867@gmail.com>
2018-03-03 14:29:55 +0200

    Update RC car.ino

--- Team 1/RC car.ino
+++ Team 1/RC car.ino
@@ -4,6 +4,13 @@ int motorPin3 = 5;
 int motorPin4 = 6;
 
 void setup() {
+ 
+pinMode(motorPin1,OUTPUT); 
+pinMode(motorPin2,OUTPUT); 
+pinMode(motorPin3,OUTPUT); 
+pinMode(motorPin4,OUTPUT); 
+
+  
 }
 
 void loop() {
