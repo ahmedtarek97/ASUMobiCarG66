b894b54bbb49452599ffdec82b1f6a6b816b3df8
AhmedGalal21 <ahmedmedo867@gmail.com>
2018-03-04 21:28:11 +0200

    Update RC car.ino

--- Team 1/RC car.ino
+++ Team 1/RC car.ino
@@ -27,6 +27,27 @@ void loop()
    digitalWrite(motorPin4,LOW); 
  } 
  
+   else if (data == '2')       //move backwards 
+ { 
+   digitalWrite(motorPin1,LOW); 
+   digitalWrite(motorPin2,HIGH); 
+   digitalWrite(motorPin3,LOW); 
+   digitalWrite(motorPin4,HIGH); 
+ } 
+   else if (data == '3')       //move forward left 
+ { 
+   digitalWrite(motorPin1,LOW); 
+   digitalWrite(motorPin2,HIGH); 
+   digitalWrite(motorPin3,HIGH); 
+   digitalWrite(motorPin4,LOW); 
+ } 
+   else if (data == '4')       //move forward right 
+ { 
+   digitalWrite(motorPin1,HIGH); 
+   digitalWrite(motorPin2,LOW); 
+   digitalWrite(motorPin3,LOW); 
+   digitalWrite(motorPin4,HIGH); 
+ } 
 
-  } 
+  }
 }
