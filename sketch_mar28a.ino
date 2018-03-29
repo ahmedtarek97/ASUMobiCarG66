1e6747445c86647a10c9066b455565d814f7b401
Ahmed Mohamed <eng.ahmed3li99.9898@gmail.com>
2018-03-29 19:10:14 +0200

    Add files via upload

---
+++ sketch_mar28a.ino
@@ -0,0 +1,25 @@
+
+const int temp1 = A0 ;
+
+const int temp1 = A1 ;
+
+const int temp1 = A2 ;
+const int temp1 = 13 ;
+cons
+
+
+
+void setup() {
+  
+ pinMode (puls,OUTPUT);
+}
+
+void loop() {
+  
+digitalWrite (puls,1);
+delay (500) ;
+digitalWrite (puls,0);
+delay (1500) ;
+
+
+}
