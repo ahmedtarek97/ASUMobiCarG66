98c8e6639077bd660439bc36a89583ce5a03bfdd
Ahmed Talaat <36507275+AHMED-3RFA@users.noreply.github.com>
2018-03-30 01:07:35 +0200

    sheet 1 p 2

---
+++ sketch_mar30a.ino
@@ -0,0 +1,40 @@
+void setup() {
+ 
+    pinMode (4,OUTPUT);
+    pinMode (5,OUTPUT);
+    pinMode (6,OUTPUT);
+    pinMode (7,OUTPUT);
+             }
+
+void loop() {
+ 
+ 
+    
+      digitalWrite (4,HIGH);
+      delay  (1000);
+      
+      digitalWrite (4,LOW);
+      delay  (3000);
+      
+    
+      digitalWrite (5,HIGH);
+      delay  (1000);
+      
+      digitalWrite (5,LOW);
+      delay  (2000);
+
+      
+      digitalWrite (6,HIGH);
+      delay  (1000);
+      
+      digitalWrite (6,LOW);
+      delay  (1000);
+
+      
+      digitalWrite (7,HIGH);
+      delay  (1000);
+      
+     
+ 
+
+}
