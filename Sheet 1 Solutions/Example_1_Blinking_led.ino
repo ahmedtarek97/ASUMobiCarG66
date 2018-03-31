18c6108a3f4f12a2ed2ad1993f2b78e1c0402faf
Ahmed Mohamed <eng.ahmed3li99.9898@gmail.com>
2018-03-31 11:43:57 +0200

    Add files via upload

---
+++ Sheet 1 Solutions/Example_1_Blinking_led.ino
@@ -0,0 +1,18 @@
+// Example 01 : Blinking LED
+
+const int LED = 13;             
+                                                    
+void setup ( )
+{
+  pinMode(LED, OUTPUT);                                                       
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);     
+  delay(1000); 
+                        
+  digitalWrite(LED, LOW);   
+  delay(1000);                          
+}
+
