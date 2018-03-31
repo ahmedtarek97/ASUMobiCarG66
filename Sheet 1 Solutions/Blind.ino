a013703fd46ceb0c385998ecc06d70a3b37a0924
Ahmed Mohamed <eng.ahmed3li99.9898@gmail.com>
2018-03-31 11:46:18 +0200

    Update Blind.ino

--- Sheet 1 Solutions/Blind.ino
+++ Sheet 1 Solutions/Blind.ino
@@ -1,11 +1,11 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   
+  digitalWrite(13, HIGH);   
   delay(1000);                       
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);    
+  digitalWrite(13, LOW);    
   delay(1000);                      
 }
