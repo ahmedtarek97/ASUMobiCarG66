973fdebda524c3a418b7ca0dfe4cd809726dbe37
Ahmad_Abdelsalam <31441716+AhmadAbdelsalam@users.noreply.github.com>
2018-03-31 02:29:26 -0700

    Using DHT11 to measure humidity

---
+++ humidity_sensor_DHT11_.ino
@@ -0,0 +1,21 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.println("C");
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(2000);
+}
+
