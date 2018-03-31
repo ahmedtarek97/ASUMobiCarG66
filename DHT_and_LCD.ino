706fd39e8448e0fab34f292e1876f3f9caeab863
Ahmad_Abdelsalam <31441716+AhmadAbdelsalam@users.noreply.github.com>
2018-03-31 02:34:12 -0700

    DHT11 and LCD

---
+++ DHT_and_LCD.ino
@@ -0,0 +1,27 @@
+#include <dht.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  lcd.begin(16, 2);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(1000);
+}
+
