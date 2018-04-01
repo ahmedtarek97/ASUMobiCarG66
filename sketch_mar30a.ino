ad96fe9b4df1ff7e7a780680f0219cfb830fd524
ahmedtarek97 <36510613+ahmedtarek97@users.noreply.github.com>
2018-04-01 21:16:43 +0200

    Add files via upload

--- sketch_mar30a.ino
+++ sketch_mar30a.ino
@@ -1,40 +1,27 @@
+
 void setup() {
- 
-    pinMode (4,OUTPUT);
-    pinMode (5,OUTPUT);
-    pinMode (6,OUTPUT);
-    pinMode (7,OUTPUT);
-             }
+  
+pinMode(2,INPUT);
+pinMode(3,INPUT);
+pinMode(4,INPUT);
+pinMode(5,INPUT);
+pinMode(6,INPUT);
+pinMode(7,INPUT);
+pinMode(8,INPUT);
+pinMode(9,INPUT);
+pinMode(13,OUTPUT);
+}
 
+int timer = 0;
 void loop() {
- 
- 
+  
+  
+  for(int i=2;i<=9;i++)
+  {
+    timer += digitalRead(i)*pow(2,(i-2));
     
-      digitalWrite (4,HIGH);
-      delay  (1000);
-      
-      digitalWrite (4,LOW);
-      delay  (3000);
-      
-    
-      digitalWrite (5,HIGH);
-      delay  (1000);
-      
-      digitalWrite (5,LOW);
-      delay  (2000);
-
-      
-      digitalWrite (6,HIGH);
-      delay  (1000);
-      
-      digitalWrite (6,LOW);
-      delay  (1000);
-
-      
-      digitalWrite (7,HIGH);
-      delay  (1000);
-      
-     
- 
+    }
+    digitalWrite(13,HIGH);
+    delay (timer*4);
 
 }
