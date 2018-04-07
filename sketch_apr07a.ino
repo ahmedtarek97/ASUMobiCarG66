8bb417e525b39c5de17b268947cb9ddb54f60521
Ahmed Talaat <36507275+AHMED-3RFA@users.noreply.github.com>
2018-04-08 00:54:34 +0200

    Add files via upload

---
+++ sketch_apr07a.ino
@@ -0,0 +1,21 @@
+#define LED 13 
+#define BUTTON 7
+void setup()
+
+{
+   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+pinMode(BUTTON, INPUT);
+digitalWrite(BUTTON, HIGH); 
+}
+int oldValue = HIGH;
+int state = LOW;
+void loop()
+{
+  int value = digitalRead(BUTTON);
+if(value==LOW && oldValue==HIGH) 
+state = (state==LOW)?HIGH:LOW;
+digitalWrite(LED, state);
+Serial.println ("state");
+oldValue = value; 
+}
