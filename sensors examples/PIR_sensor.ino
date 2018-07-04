97e4fb01f0f275c2340f5984310b597f81ced8ba
Ahmed Sherif <36515991+ahmdshrf@users.noreply.github.com>
2018-07-04 17:04:23 +0200

    Rename PIR_sensor.ino to sensors examples/PIR_sensor.ino

---
+++ sensors examples/PIR_sensor.ino
@@ -0,0 +1,39 @@
+#define pirPin 2
+int calibrationTime = 30;
+long unsigned int lowIn;
+long unsigned int pause = 5000;
+boolean lockLow = true;
+boolean takeLowTime;
+int PIRValue = 0;
+
+void setup() {
+   Serial.begin(9600);
+   pinMode(pirPin, INPUT);
+}
+
+void loop() {
+   PIRSensor();
+}
+
+void PIRSensor() {
+   if(digitalRead(pirPin) == HIGH) {
+      if(lockLow) {
+         PIRValue = 1;
+         lockLow = false;
+         Serial.println("Motion detected.");
+         delay(50);
+      }
+      takeLowTime = true;
+   }
+   if(digitalRead(pirPin) == LOW) {
+      if(takeLowTime){
+         lowIn = millis();takeLowTime = false;
+      }
+      if(!lockLow && millis() - lowIn > pause) {
+         PIRValue = 0;
+         lockLow = true;
+         Serial.println("Motion ended.");
+         delay(50);
+      }
+   }
+}
