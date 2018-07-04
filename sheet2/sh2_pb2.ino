e1f5534e89db36c90c51cad9f611cf2f74a38bee
Ahmed Sherif <36515991+ahmdshrf@users.noreply.github.com>
2018-07-04 15:45:53 +0200

    Rename sh2_pb2.ino to sheet2/sh2_pb2.ino

---
+++ sheet2/sh2_pb2.ino
@@ -0,0 +1,32 @@
+void setup(){
+pinMode(2,INPUT);  
+pinMode(3,INPUT);
+pinMode(4,OUTPUT);
+attachInterrupt(digitalPinToInterrupt(2), faster , RISING);
+attachInterrupt(digitalPinToInterrupt(3), slower , RISING);
+}
+
+int x=1000;
+void loop() {
+
+  digitalWrite(4,HIGH);
+  delay(x);
+  digitalWrite(4,LOW);
+  delay(x);
+ 
+}
+
+void slower()
+{
+  
+  if(x<10000)
+  x+=1000;
+  
+  }
+  void faster()
+{
+  
+  if(x>1000)
+  x-=1000;
+  
+  }
